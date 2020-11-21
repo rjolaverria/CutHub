106a5b4b51647a4c58b909bcf219b495fc71d14d
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-21 13:23:09 -0500

    update server url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
     process.env.NODE_ENV === 'production'
         ? 'https://cut-hub.herokuapp.com/'
-        : '/';
+        : 'http://localhost:5000/';
 
 export default axios.create({
     baseURL,
